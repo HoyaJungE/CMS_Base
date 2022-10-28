9de7cc45382846bea210e53df8b82a603f0d9772
한지호 <hanho1312@gmail.com>
2022-10-28 16:37:38 +0900

    기본설정사항 수정

---
+++ src/main/java/cmsBase/main/web/MainController.java
@@ -0,0 +1,15 @@
+package cmsBase.main.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/main.do")
+    public String index(ModelMap model) {
+        return "cmsBase/main/UnitMain";
+    }
+
+}
